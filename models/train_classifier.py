a1d1f604f6a1472daf5f19812bec1bec2bb32b76
joaoalbertofs <joaoalberto.sequeira@gmail.com>
2023-08-15 23:39:42 -0300

    avancando

---
+++ models/train_classifier.py
@@ -0,0 +1,126 @@
+
+
+
+import sys
+
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer, classification_report
+import numpy as np
+import pickle
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table(database_filepath, engine)
+    
+    X = df['message']
+    y = df.iloc[:,4:]
+    category_names = y.columns
+    
+    return X, y, category_names
+
+
+def tokenize(text,url_place_holder_string="urlplaceholder"):
+
+    # Replace all urls with a urlplaceholder string
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    # Extract all the urls from the provided text 
+    detected_urls = re.findall(url_regex, text)
+    
+    # Replace url with a url placeholder string
+    for detected_url in detected_urls:
+        text = text.replace(detected_url, url_place_holder_string)
+
+    # Extract the word tokens from the provided text
+    tokens = nltk.word_tokenize(text)
+    
+    #Lemmanitizer to remove inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    # List of clean tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+    return clean_tokens
+
+
+def build_model():
+        pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+        
+        parameters = {
+        'tfidf__smooth_idf': [True],
+        'clf__estimator__n_estimators': [40],
+        }
+        
+        model = GridSearchCV(pipeline, parameters)
+        
+        return model
+        
+           
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+    
+    for i, column in enumerate(category_names):
+        print(f"Category: {column}\n")
+        print(classification_report(Y_test[column], y_pred[:, i]))
+        print("-" * 60)
+       
+
+
+
+def save_model(model, model_filepath):
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
+
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
