f861dc8d23f05b4ac3bf1f4ca5f64bc7411c263a
joaoalbertofs <joaoalberto.sequeira@gmail.com>
2023-08-15 23:42:16 -0300

    ajuste arquivo

--- data/process_data.py
+++ data/process_data.py
@@ -1,119 +1,65 @@
 import sys
-
-# import libraries
 import pandas as pd
 from sqlalchemy import create_engine
-import re
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer, classification_report
 import numpy as np
-import pickle
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on = 'id')
+    return df 
 
 
-import nltk
-nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
-
-
-def load_data(database_filepath):
-    engine = create_engine('sqlite:///'+database_filepath)
-    df = pd.read_sql_table(database_filepath, engine)
+def clean_data(df):
+    # Split the categories
+    categories = df['categories'].str.split(pat=';',expand=True)
     
-    X = df['message']
-    y = df.iloc[:,4:]
-    category_names = y.columns
+    #Fix the categories columns name
+    row = categories.iloc[[1]]
+    category_colnames = [category_name.split('-')[0] for category_name in row.values[0]]
+    categories.columns = category_colnames
     
-    return X, y, category_names
-
-
-def tokenize(text,url_place_holder_string="urlplaceholder"):
-    # Replace all urls with a urlplaceholder string
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(np.int)
     
-    # Extract all the urls from the provided text 
-    detected_urls = re.findall(url_regex, text)
+    df = df.drop('categories',axis=1)
+    df = pd.concat([df,categories],axis=1)
+    df = df.drop_duplicates()
     
-    # Replace url with a url placeholder string
-    for detected_url in detected_urls:
-        text = text.replace(detected_url, url_place_holder_string)
-
-    # Extract the word tokens from the provided text
-    tokens = nltk.word_tokenize(text)
-    
-    #Lemmanitizer to remove inflectional and derivationally related forms of a word
-    lemmatizer = nltk.WordNetLemmatizer()
-
-    # List of clean tokens
-    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
-    return clean_tokens
-
-
-def build_model():
-        pipeline = Pipeline([
-        ('vect', CountVectorizer(tokenizer=tokenize)),
-        ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(RandomForestClassifier()))
-        ])
-        
-        parameters = {
-        'tfidf__smooth_idf': [True],
-        'clf__estimator__n_estimators': [40],
-        }
-        
-        model = GridSearchCV(pipeline, parameters)
-        
-        return model
-        
-           
-def evaluate_model(model, X_test, Y_test, category_names):
-        y_pred = model.predict(X_test)
-        classif_report = classification_report(Y_test, y_pred, target_names=category_names)
-        for i, column in enumerate(y.columns):
-            print(f"Category: {column}\n")
-            print(classification_report(y_test[column], y_pred[:, i]))
-            print("-" * 60)
-       
+    return df
 
 
 
-def save_model(model, model_filepath):
-    with open(model_filepath, 'wb') as file:
-        pickle.dump(model, file)
-
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
-        print('Building model...')
-        model = build_model()
-        
-        print('Training model...')
-        model.fit(X_train, Y_train)
-        
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+    if len(sys.argv) == 4:
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Trained model saved!')
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
 
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
 
 
 if __name__ == '__main__':
